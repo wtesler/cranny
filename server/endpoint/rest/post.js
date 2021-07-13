b0481f21b23e9f2aa20e4135505a8c6dd62bfb8e
Will Tesler <willtesler@gmail.com>
2021-07-12 21:02:06 -0500

    merge simplePost into post.

--- server/endpoint/rest/post.js
+++ server/endpoint/rest/post.js
@@ -10,36 +10,48 @@ const rest = require("./rest");
 module.exports = function (handler) {
   return rest('POST', async (req, res) => {
     return new Promise((resolve, reject) => {
-
-      const buffers = [];
-      let length = 0;
-
-      req.on("data", chunk => {
-        buffers.push(chunk);
-        length += chunk.length;
-      });
-
-      req.on("end", async () => {
-        req.body = Buffer.concat(buffers, length).toString();
-        if (req.body) {
-          req.rawBody = req.body;
-          try {
-            // Since this case is so common, we try to handle it here.
-            req.body = JSON.parse(req.body);
-          } catch (e) {
-            // Everything is fine, we're just not dealing with a JSON body.
-          }
-        } else {
-          req.body = {};
-        }
-
+      const run = async() => {
         try {
           const response = await handler(req, res);
           resolve(response);
         } catch (e) {
           reject(e);
         }
-      });
+      }
+
+      if (req.body) {
+        run();
+      } else {
+        const buffers = [];
+        let length = 0;
+
+        req.on("data", chunk => {
+          buffers.push(chunk);
+          length += chunk.length;
+        });
+
+        req.on("end", async () => {
+          req.body = Buffer.concat(buffers, length).toString();
+          if (req.body) {
+            req.rawBody = req.body;
+            try {
+              // Since this case is so common, we try to handle it here.
+              req.body = JSON.parse(req.body);
+            } catch (e) {
+              // Everything is fine, we're just not dealing with a JSON body.
+            }
+          } else {
+            req.body = {};
+          }
+
+          try {
+            const response = await handler(req, res);
+            resolve(response);
+          } catch (e) {
+            reject(e);
+          }
+        });
+      }
     });
   });
 };
