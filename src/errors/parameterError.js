9915a4c192968d4094c389b3c55125a4a79c5221
Will Tesler <willtesler@gmail.com>
2023-03-31 13:44:57 -0500

    Handle missing and malformed parameters error

--- src/errors/parameterError.js
+++ src/errors/parameterError.js
@@ -1,11 +1,13 @@
-module.exports = (req=null) => {
+module.exports = (req=null, isMissing=true) => {
+  const problem = isMissing ? 'Missing' : 'Malformed';
+
   if (req) {
-    console.warn("Missing or malformed parameters in the following body");
+    console.warn(`${problem} parameters in the following body`);
     console.warn(req.body);
   }
 
   const e = new Error();
   e.code = 422;
-  e.message = "Some parameters are missing or malformed.";
+  e.message = `Some parameters are ${problem.toLowerCase()}.`;
   throw e;
 };
