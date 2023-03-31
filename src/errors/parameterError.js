f26a284613ceba5dedbbc68a0069817b8d429e68
Will Tesler <willtesler@gmail.com>
2023-03-31 13:42:07 -0500

    Clearer messaging in parameterError

--- src/errors/parameterError.js
+++ src/errors/parameterError.js
@@ -1,16 +1,11 @@
 module.exports = (req=null) => {
   if (req) {
-    if (req.method === "POST") {
-      console.log("Missing parameters in the following body");
-      console.log(req.body);
-    } else if (req.method === "GET") {
-      console.log("Missing parameters in the following query");
-      console.log(req.query);
-    }
+    console.warn("Missing or malformed parameters in the following body");
+    console.warn(req.body);
   }
 
   const e = new Error();
   e.code = 422;
-  e.message = "Missing some parameters.";
+  e.message = "Some parameters are missing or malformed.";
   throw e;
 };
