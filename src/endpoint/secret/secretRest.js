360e6c149ace5f8e51b2001aee446e474220f4cb
Will Tesler <willtesler@gmail.com>
2023-06-15 18:43:28 -0500

    Add secretRest

---
+++ src/endpoint/secret/secretRest.js
@@ -0,0 +1,21 @@
+/**
+ * Turns the provided handler function into a secret HTTPS REST function.
+ * The function is private-key protected by a shared secret.
+ *
+ * @param handler A function which takes (req, res).
+ * @param secret The secret to compare the passed-in secret with.
+ */
+module.exports = function(handler, secret) {
+  const rest = require('../rest/rest');
+
+  return rest(async (req, res) => {
+    const _secret = req.body._secret;
+
+    if (!_secret || _secret !== secret) {
+      res.status(403).send({ code: 403, message: 'Access Denied.' });
+      return Promise.resolve();
+    }
+
+    return await handler(req, res);
+  });
+};
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ const endpoint = require('./endpoint/endpoint');
 const accessError = require('./errors/accessError');
 const parameterError = require('./errors/parameterError');
 const discoverEndpoints = require('./host/discoverEndpoints');
+const secretRest = require('./endpoint/secret/secretRest');
 
 module.exports = {
   rest: rest,
   endpoint: endpoint,
   accessError: accessError,
   parameterError: parameterError,
-  discoverEndpoints: discoverEndpoints
+  discoverEndpoints: discoverEndpoints,
+  secretRest: secretRest
 }
